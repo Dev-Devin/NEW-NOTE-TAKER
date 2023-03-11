f45233dc5ee7a6ec25d534c48ee2f6fdbdd42021
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-10 20:10:30 -0500

    new files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const htmlRouter = require("express").Router();
+const path = require("path");
+
+// GET - homepage
+htmlRouter.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+// GET - notes
+htmlRouter.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+);
+
+module.exports = htmlRouter;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { route } = require("./htmlRoutes");
+const htmlRoutes = require("./htmlRoutes");
+const apiRoutes = require("./api");
+
+// HTML ROUTES
+router.use(htmlRoutes);
+
+// API ROUTES
+router.use("/api", apiRoutes);
+
+module.exports = router;
