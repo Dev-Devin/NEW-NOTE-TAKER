5159de7bfbe34fda010bc79deb933356b64c81ee
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-13 08:50:19 -0400

    added apiroutes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -3,7 +3,13 @@ const path = require("path");
 const fs = require("fs");
 console.log("dirname= ", __dirname);
 router.get("/", (req, rest) => {
-  // rest.sendFile(path.join(__dirname, ))
+  console.log("get all notes");
+  rest.sendFile(path.join(__dirname, "../../db/db.json"));
 });
 
+router.get("/:id", (req, rest) => {
+  console.log("get one notes");
+  let notes = JSON.parse(fs.readFileSync("../../db/db.json", "utf-8"));
+  rest.json(notes[Number(req.params.id)]);
+});
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,15 @@ const htmlRouter = require("express").Router();
 const path = require("path");
 
 // GET - homepage
-htmlRouter.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
-);
+htmlRouter.get("/", (req, res) => {
+  console.log("/ route");
+  res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+});
 
 // GET - notes
-htmlRouter.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/notes.html"))
-);
+htmlRouter.get("/notes", (req, res) => {
+  console.log("/ notesroute");
+  res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
+});
 
 module.exports = htmlRouter;
