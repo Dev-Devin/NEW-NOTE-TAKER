77049df5d2292877243049c95f2fdc7e9866a1c9
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-13 08:54:22 -0400

    getting local host

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,13 +4,13 @@ const path = require("path");
 // GET - homepage
 htmlRouter.get("/", (req, res) => {
   console.log("/ route");
-  res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // GET - notes
 htmlRouter.get("/notes", (req, res) => {
   console.log("/ notesroute");
-  res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 module.exports = htmlRouter;
